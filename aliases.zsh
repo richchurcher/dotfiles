d6b2c010ba8694ff5a21ad994fcb27c9ecc0a5f4
Rich Churcher <rich.churcher@gmail.com>
2020-09-01 16:04:21 +1200

    zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,22 +1,15 @@
-# Listin' stuff
-alias ls='ls --color'
-alias ll='ls -la'
-
-# Proton VPN
-alias vpn='sudo openvpn /etc/openvpn/client/protonvpn-ca4.conf'
-
 # Git
-alias gs='git status'
-alias gc='git checkout'
-alias gcb='git checkout -b'
-alias gm='git commit -m'
-alias gma='git commit -am'
-alias gp='git push'
-alias gl='git log'
+# alias gs='git status'
+# alias gc='git checkout'
+# alias gcb='git checkout -b'
+# alias gm='git commit -m'
+# alias gma='git commit -am'
+# alias gp='git push'
+# alias gl='git log'
 
 # Wifi
-alias 2g='sudo netctl-auto switch-to 2g'
-alias 5g='sudo netctl-auto switch-to 5g'
+alias 2g='sudo netctl-auto switch-to home2g'
+alias 5g='sudo netctl-auto switch-to home5g'
 alias eda='sudo netctl-auto switch-to EDA-5G'
 alias mobile='sudo netctl-auto switch-to mobile'
 alias ping8='ping 8.8.8.8'
@@ -25,16 +18,13 @@ alias ping8='ping 8.8.8.8'
 alias vim='nvim'
 alias e='nvim'
 
-# Emacs
-alias emacs='emacs -nw'
-
 # Display
 alias dell='xrandr --output DP-0.2.8 --right-of DP-2'
 alias portrait='xrandr --output DP-0 --mode 3840x2160 --rotate left --above DP-2'
 alias landscape='xrandr --output DP-0.8 --mode 3840x2160 --right-of DP-2'
 alias thinkpad='xrandr --output DP-2 --mode 1920x1080 --panning 1920x1080'
 alias plandscape='xrandr --output DP-0.8 --mode 3840x2160 --above DP-2 --output DP-0.1 --mode 3840x2160 --rotate left --left-of DP-2'
-alias home='xrandr --output DP-0.8 --mode 3840x2160 --left-of DP-0.1 --output DP-0.1 --mode 3840x2160 --rotate right --left-of DP-2' 
+alias home='xrandr --output DP-0.8 --mode 3840x2160 --output DP-0.1 --mode 3840x2160 --rate 30 --rotate right --right-of DP-0.8 --output DP-2 --mode 1920x1080 --right-of DP-0.1'
 alias generator='xrandr --output DP-4 --mode 1920x1080 --left-of DP-2'
 
 # Record
@@ -45,13 +35,20 @@ alias record=$'amixer set Capture 35% && \
   -f alsa -thread_queue_size 512 -ac 2 -i hw:0 \
   -preset ultrafast \'output.mkv\''
 
+alias record-portrait=$'amixer set Capture 35% && \
+  ffmpeg -s 2160x1920 \
+  -f x11grab -thread_queue_size 512 -probesize 10MB -r 25 -i :0.0+3840 \
+  -f alsa -thread_queue_size 512 -ac 2 -i hw:0 \
+  -af "afftdn" \
+  \'output.mkv\''
+
 # With headset, landscape screen
-alias record4k=$'amixer set Capture 100% && \
-  ffmpeg -y \
-  -f alsa -thread_queue_size 512 -ac1 -i hw:1 \
-  -f x11grab -thread_queue_size 512 -probesize 10MB -framerate 30 -video_size 3840x2160 -i :0.0+0,0 \
-  -c:v libx264 -pix_fmt yuv420p -qp 0 \
-  -preset ultrafast \'output.mkv\''
+# alias record4k=$'amixer set Capture 100% && \
+#   ffmpeg -y \
+#   -f alsa -thread_queue_size 512 -ac1 -i hw:1 \
+#   -f x11grab -thread_queue_size 512 -probesize 10MB -framerate 30 -video_size 3840x2160 -i :0.0+0,0 \
+#   -c:v libx264 -pix_fmt yuv420p -qp 0 \
+#   -preset ultrafast \'output.mkv\''
 
 # sqlite3
 alias sqlite3='sqlite3 -init "${XDG_CONFIG_HOME}"/sqlite3/sqliterc'
@@ -70,21 +67,30 @@ alias mute='amixer set Master 0'
 alias unmute='amixer set Master 100%'
 
 # places
-alias apollo='cd /w/lic/apollo-ui && nvm use'
-alias loffty='cd /w/loffty/ui && nvm use'
-alias lapi='cd /w/loffty/api && nvm use'
 alias lunchbunch='cd /w/lunchbunch/'
 alias hex='cd /w/hexjam/'
+alias bsui='cd /w/bs/ui && nvm use'
+alias bsapi='cd /w/bs/api && nvm use'
+alias foo='cd /w/foo && nvm use'
+alias aoc='cd /w/advent-of-code-2019'
+alias blog='cd /w/blog'
+alias vly='cd /w/vly2 && nvm use'
+alias rt='cd /w/reno-tracker && nvm use && export AWS_PROFILE=renotracker'
 
 # docker-compose
 alias dc='docker-compose'
-alias dcf='dc -f dev/docker-compose.yml'
-alias dcu='dcf run -d --service-ports -T --name loffty-api-dev-container api npm run api'
-alias dcd='dcf down'
-alias dcr='dcd && dcu && dcs'
-alias dct='docker exec -it loffty-api-dev-container npm run test:watch'
-alias dcc='docker exec -it loffty-api-dev-container bash'
-alias dcs='docker exec loffty-api-dev-container npm run db:seed'
-alias dcdeploy='dcf run --rm --no-deps api npm run deploy:accp'
-alias dldb='docker logs -f -t loffty-dynamodb'
-alias dlapi='docker logs -f -t loffty-api-dev-container'
+alias dcu='dc up -d'
+alias dcd='dc down'
+
+# mail
+alias notmuch='notmuch --config="${XDG_CONFIG_HOME}"/notmuch/config'
+alias getmail='getmail -g "${XDG_CONFIG_HOME}"/getmail'
+alias alot='alot -n "${XDG_CONFIG_HOME}"/notmuch/config'
+
+# project-specific
+alias bsdb='pgcli postgres://qstoolbox@localhost/qstoolbox_dev'
+alias bsdb-prod='pgcli postgres://qstoolbox@pg-1688156c.field-7958.aivencloud.com:10412/qstoolbox?ssl=true'
+alias rtdb='pgcli postgres://postgres:renotracker@localhost/renotracker_dev'
+
+# fbnohkqrctm0heym
+
